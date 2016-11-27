46f6e15953c69d61dbbe05adcca1bf27df85fa64
eugenio79 <giuseppe.morana.79@gmail.com>
2016-11-27 21:47:37 +0100

    Added test file for PostDetailController

---
+++ BabylonHealthDemo/BabylonHealthDemoTests/PostDetail/Controllers/PostDetailControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  PostDetailControllerTests.swift
+//  BabylonHealthDemo
+//
+//  Created by Giuseppe Morana on 27/11/2016.
+//  Copyright © 2016 Giuseppe Morana. All rights reserved.
+//
+
+import XCTest
+
+class PostDetailControllerTests: XCTestCase {
+    
+    
+}
